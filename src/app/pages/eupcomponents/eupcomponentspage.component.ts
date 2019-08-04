26b095b6da2c596c433b4f67c453b3805d1639f1
Tzion Tzur <ttzur@aligntech.com>
2019-08-04 16:20:51 +0300

    add eup theme to eup component

--- src/app/pages/eupcomponents/eupcomponentspage.component.ts
+++ src/app/pages/eupcomponents/eupcomponentspage.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+export interface Animal {
+  name: string;
+  sound: string;
+}
 @Component({
   selector: 'app-testpage',
   templateUrl: './eupcomponentspage.component.html',
   styleUrls: ['./eupcomponentspage.component.scss']
 })
-export class EupcomponentspageComponent implements OnInit {
+export class EupcomponentspageComponent {
+  animal: string;
+  name: string;
 
-  constructor() { }
+  myAanimal: string;
+  myName: string;
 
-  ngOnInit() {
-  }
+  animalControl = new FormControl('', [Validators.required]);
+  selectFormControl = new FormControl('', Validators.required);
+  animals: Animal[] = [
+    { name: 'Dog', sound: 'Woof!' },
+    { name: 'Cat', sound: 'Meow!' },
+    { name: 'Cow', sound: 'Moo!' },
+    { name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!' },
+  ];
+  constructor() {}
 
 }
