e30cb2e2c7c42d6f3fe90fc0a9e1f1a7568ddd30
Tzion Tzur <ttzur@aligntech.com>
2019-08-14 11:30:14 +0300

    create dialog close buttun

---
+++ src/app/dreupcomponents/dr-dialog-content/dr-dialog-close-button/dr-dialog-close-button.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'dr-dialog-close-button',
+  templateUrl: './dr-dialog-close-button.component.html',
+  styleUrls: ['./dr-dialog-close-button.component.scss']
+})
+export class DrDialogCloseButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  @Input()
+	public useHandler(handler: Function): void {
+		// If the handler needs its original context (this), the handler MUST be an arrow function
+		handler();
+  }
+
+  @Input()
+	public clickHandler: Function;
+
+}
--- src/app/dreupcomponents/dr-dialog-content/dr-dialog-content.component.ts
+++ src/app/dreupcomponents/dr-dialog-content/dr-dialog-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'dr-dialog-content',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DrDialogContentComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public dialogRef:MatDialogRef<DrDialogContentComponent>) { }
+  closeDialog = () => {
+    this.dialogRef.close();
+  }
+  
   ngOnInit() {
   }
 
